bae3aeaf205adaaf3ad6fc11f41efb1595be9e4c
igporto <igport@esei.uvigo.es>
2016-12-29 12:47:20 +0100

    mini erro na comprobacion do que che chegaba por post no edit de usuario

--- controller/USER_controller.php
+++ controller/USER_controller.php
@@ -226,7 +226,7 @@ class UserController extends BaseController {
 			$user = $this->userMapper->view($user_id);
 
 			//Engadimos o novo contrasinal ao usuario se chega (se non deixamos o que ten)
-			if(isset($_POST["newpass"])&& addslashes($_POST['newpass'])!=""){
+			if(isset($_POST["newpass"])&& $_POST['newpass']!=""){
 				$user->setPasswd(md5(htmlentities(addslashes($_POST["newpass"]))));
 			}else{
 				$pass = $user->getPasswd();
