48149b5a15ec9338503ffc364278cf010ac10cba
Adrián Simón <adriansimn15@gmail.com>
2017-01-14 19:08:34 +0100

    fix

---
+++ core/language/parsley/gl.js
@@ -0,0 +1,27 @@
+
+Parsley.addMessages('gl', {
+  defaultMessage: "Este valor parece ser inválido.",
+  type: {
+    email:        "Este valor debe ser un correo válido.",
+    url:          "Este valor debe ser unha URL válida.",
+    number:       "Este valor debe ser un número válido.",
+    integer:      "Este valor debe ser un número válido.",
+    digits:       "Este valor debe ser un díxito válido.",
+    alphanum:     "Este valor debe ser alfanumérico."
+  },
+  notblank:       "Este valor non debe estar en branco.",
+  required:       "Este valor é requirido.",
+  pattern:        "Este valor é incorrecto.",
+  min:            "Este valor non debe ser menor que %s.",
+  max:            "Este valor non debe ser maior que %s.",
+  range:          "Este valor debe estar entre %s y %s.",
+  minlength:      "Este valor é moi corto. A lonxitude máxima é de %s caracteres.",
+  maxlength:      "Este valor é moi largo. A lonxitude máxima é de %s caracteres.",
+  length:         "A lonxitude deste valor debe estar entre %s e %s caracteres.",
+  mincheck:       "Debe seleccionar cando menos %s opcións.",
+  maxcheck:       "Debe seleccionar %s opcións ou menos.",
+  check:          "Debe seleccionar entre %s e %s opcións.",
+  equalto:        "Este valor debe ser idéntico."
+});
+
+Parsley.setLocale('gl');
